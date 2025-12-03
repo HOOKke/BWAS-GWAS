b5afab6b295e0927a7915691d3aaab4393e46d80
Ke Hu <1013746807@qq.com>
2025-12-03 15:37:02 +0800

    Add files via upload

---
+++ Analysis/Post-GWAS/Genetic_correlations/Reaction_time/step1.sh
@@ -0,0 +1,7 @@
+/mnt/data0/home/khu/ldsc/munge_sumstats.py \
+  --sumstats Davies2018_UKB_RT_summary_results_29052018.txt \
+  --merge-alleles /mnt/data0/home/khu/ldsc/eur_w_ld_chr/w_hm3.snplist \
+  --N 300486 \
+  --out RT
+
+
---
+++ Analysis/Post-GWAS/Genetic_correlations/SCZ/step1.sh
@@ -0,0 +1,6 @@
+/mnt/data0/home/khu/ldsc/munge_sumstats.py \
+  --sumstats /mnt/data0/home/khu/UKB_GWAS/CCA_combine/mental_combine/GWAS_IDP/GWAS_2/LDSC/gene_corr/SZ/PGC3_SCZ_wave3.european.autosome.public.v3.vcf.tsv \
+  --merge-alleles /mnt/data0/home/khu/ldsc/eur_w_ld_chr/w_hm3.snplist \
+  --out PGC3_SCZ
+
+
