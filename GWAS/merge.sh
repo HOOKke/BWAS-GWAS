63cde7bb04084f76718287004edccb2a7a543a4a
Ke Hu <1013746807@qq.com>
2025-12-03 14:16:56 +0800

    Delete GWAS directory

--- GWAS/merge.sh
+++
@@ -1,6 +0,0 @@
-# merge 22 chromosome
-plink \
-  --bfile /mnt/data2/home02/khu/UKB_GWAS/CCA_combine/mental_sMRI/GWAS_IDP/snp_filter_missnp/ukb22828_c1_4\
-  --merge-list /mnt/data2/home02/khu/UKB_GWAS/CCA_combine/mental_sMRI/GWAS_IDP/snp_filter_missnp/merge_list.txt \
-  --make-bed \
-  --out /mnt/data2/home02/khu/UKB_GWAS/CCA_combine/mental_sMRI/GWAS_IDP/GWAS/merge
--- GWAS/sample_filter_nosex.sh
+++
@@ -1,7 +0,0 @@
-for i in `seq 1 1 1`; do
-  echo "$i"
-  plink --bfile /mnt/data0/home/khu/UKB_GWAS/Gradient_neuroticism/sample_filter_mind/ukb_22828_c"$i"_2 \
-  --keep /mnt/data2/home02/khu/UKB_GWAS/CCA_combine/mental_sMRI/GWAS_IDP/sample_filter_gene_mental/eid_gene_img.txt \
-  --make-bed \
-  --out /mnt/data2/home02/khu/UKB_GWAS/CCA_combine/mental_sMRI/GWAS_IDP/sample_filter_gene_mental/ukb22828_c"$i"_3
-done
