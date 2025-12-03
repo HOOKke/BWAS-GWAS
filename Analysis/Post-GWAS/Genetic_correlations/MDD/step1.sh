1d88d9b30ca97aaab0cbc9af9dfa174b88b978fb
Ke Hu <1013746807@qq.com>
2025-12-03 15:36:00 +0800

    Add files via upload

---
+++ Analysis/Post-GWAS/Genetic_correlations/MDD/step1.sh
@@ -0,0 +1,8 @@
+/mnt/data0/home/khu/ldsc/munge_sumstats.py \
+  --sumstats PGC_UKB_depression_genome-wide.txt \
+  --merge-alleles /mnt/data0/home/khu/ldsc/eur_w_ld_chr/w_hm3.snplist \
+  --signed-sumstats LogOR,0 \
+  --N 500199 \
+  --out ./MDD2019 
+
+
---
+++ Analysis/Post-GWAS/Genetic_correlations/PTSD/step1.sh
@@ -0,0 +1,8 @@
+/mnt/data0/home/khu/ldsc/munge_sumstats.py \
+  --sumstats pts_eur_freeze2_overall.results \
+  --merge-alleles /mnt/data0/home/khu/ldsc/eur_w_ld_chr/w_hm3.snplist \
+  --N-cas-col Nca \
+  --N-con-col Nco \
+  --out ptsd
+
+
