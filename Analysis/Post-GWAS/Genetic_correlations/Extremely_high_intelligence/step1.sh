f39e3ef5c56cfec65e97fee9f5a9e388b0f2009b
Ke Hu <1013746807@qq.com>
2025-12-03 15:30:59 +0800

    Add files via upload

---
+++ Analysis/Post-GWAS/Genetic_correlations/Extremely_high_intelligence/step1.sh
@@ -0,0 +1,8 @@
+/mnt/data0/home/khu/ldsc/munge_sumstats.py \
+  --sumstats Coleman_29520040_Sumstats.txt \
+  --merge-alleles /mnt/data0/home/khu/ldsc/eur_w_ld_chr/w_hm3.snplist \
+  --N-cas 1247 \
+  --N-con 86493 \
+  --out Extremely_high_intelligence1
+
+
---
+++ Analysis/Post-GWAS/Genetic_correlations/Intelligence/step1.sh
@@ -0,0 +1,8 @@
+/mnt/data0/home/khu/ldsc/munge_sumstats.py \
+  --sumstats SavageJansen_2018_intelligence_metaanalysis.txt \
+  --merge-alleles /mnt/data0/home/khu/ldsc/eur_w_ld_chr/w_hm3.snplist \
+  --frq EAF_HRC \
+  --N-col N_analyzed \
+  --out intelligence 
+
+
